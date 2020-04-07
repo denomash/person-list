2ec44fc6cbf121effd360f2910911562c6168785
denomash <dennismacharia4747@gmail.com>
2020-04-07 03:52:53 -0400

    initial setup

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+const PersonList = () => {
+  return (
+    <section>
+      <div>
+         Content will go here
+      </div>
+    </section>
+  )
+}
+
+ReactDOM.render(
+  <PersonList />,
+  document.getElementById('root')
+);
